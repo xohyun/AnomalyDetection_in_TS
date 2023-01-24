a5fda74c550b29820222057da329d646e5411fba
xohyun <bluecow50@gmail.com>
2023-01-24 03:36:44 -1000

    fix ~ing

--- Model/OmniAnomaly.py
+++ Model/OmniAnomaly.py
@@ -3,19 +3,24 @@ import torch.nn as nn
 
 # OmniAnomaly (KDD 2019)
 class OmniAnomaly(nn.Module):
-    def __init__(self, feats):
+    def __init__(self, feats, seq_len):
         super(OmniAnomaly, self).__init__()
         self.beta = 0.01
         self.n_feats = feats
+        self.seq_len = seq_len
         self.n_hidden = 32
         self.n_latent = 8
-        self.lstm = nn.GRU(feats, self.n_hidden, 2)
+        self.n = self.n_feats * self.seq_len
+
+        self.lstm = nn.GRU(self.n_feats, self.n_hidden, 2, batch_first=True)
+        
         self.encoder = nn.Sequential(
             nn.Linear(self.n_hidden, self.n_hidden), nn.PReLU(),
             nn.Linear(self.n_hidden, self.n_hidden), nn.PReLU(),
-            nn.Flatten(),
+            # nn.Flatten(start_dim=1),
             nn.Linear(self.n_hidden, 2*self.n_latent)
         )
+        
         self.decoder = nn.Sequential(
             nn.Linear(self.n_latent, self.n_hidden), nn.PReLU(),
             nn.Linear(self.n_hidden, self.n_hidden), nn.PReLU(),
@@ -24,17 +29,19 @@ class OmniAnomaly(nn.Module):
     
     def forward(self, x, hidden=None):
         hidden = torch.rand(2, 1, self.n_hidden, dtype=torch.float64) if hidden is not None else hidden
-        out, hidden = self.lstm(x.view(1, 1, -1), hidden)
+        out, hidden = self.lstm(x, hidden)
 
         ## Encoder
-        x = self.encoder(out)
-        mu, logvar = torch.split(x, [self.n_latent, self.n_latent], dim=-1)
+        x = self.encoder(out) # [batch, seq_len, 16]
+        print(">>>x", x.shape)
+        mu, logvar = torch.split(x, [self.n_latent, self.n_latent], dim=-1) # [batch, seq_len, 8]
 
         ## Reparameterization trick
-        std = torch.exp(0.5*logvar)
+        std = torch.exp(0.5*logvar) # [batch, seq_len, 8]
         eps = torch.randn_like(std)
         x = mu + eps*std
 
         ## Decoder
         x = self.decoder(x)
-        return x.view(-1), mu.view(-1), logvar.view(-1), hidden
+        print(">>>?",  x.shape)
+        return x.reshape(-1), mu.reshape(-1), logvar.reshape(-1), hidden
--- Model/model_maker.py
+++ Model/model_maker.py
@@ -1,7 +1,7 @@
 from utils.utils import gpu_checking
 import os
 import pickle
-from Model import AE, DAGMM
+from Model import AE, DAGMM, OmniAnomaly
 from utils.utils import create_folder
 
 class ModelMaker:
@@ -28,6 +28,9 @@ class ModelMaker:
         elif self.args.model == 'DAGMM':
             model = DAGMM.DAGMM(self.data_info['num_features'],
                                 self.data_info['seq_len']).to(self.device)
+        elif self.args.model == 'OmniAnomaly':
+            model = OmniAnomaly.OmniAnomaly(self.data_info['num_features'],
+                                            self.data_info['seq_len']).to(self.device)
         create_folder(self.save_path)
         write_pickle(os.path.join(self.save_path, f"model_{1}.pk"), model)
         return model
