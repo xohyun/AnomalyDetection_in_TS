c13db5d7c910ac2992658fec26c981ce22c2f75d
xohyun <bluecow50@gmail.com>
2023-02-07 09:46:27 -1000

    attention

--- Model/LSTM_decom.py
+++ Model/LSTM_decom.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import math
 
 class AutoEncoder(nn.Module):
     def __init__(self, num_features, seq_len):
@@ -114,91 +115,6 @@ class PositionalEmbedding(nn.Module):
     def forward(self, x):
         return self.pe[:, :x.size(1)]
 
-class TokenEmbedding(nn.Module):
-    def __init__(self, c_in, d_model):
-        super(TokenEmbedding, self).__init__()
-        padding = 1 if torch.__version__>='1.5.0' else 2
-        self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model, 
-                                    kernel_size=3, padding=padding, padding_mode='circular')
-        for m in self.modules():
-            if isinstance(m, nn.Conv1d):
-                nn.init.kaiming_normal_(m.weight,mode='fan_in',nonlinearity='leaky_relu')
-
-    def forward(self, x):
-        x = self.tokenConv(x.permute(0, 2, 1)).transpose(1,2)
-        return x
-
-class FixedEmbedding(nn.Module):
-    def __init__(self, c_in, d_model):
-        super(FixedEmbedding, self).__init__()
-
-        w = torch.zeros(c_in, d_model).float()
-        w.require_grad = False
-
-        position = torch.arange(0, c_in).float().unsqueeze(1)
-        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
-
-        w[:, 0::2] = torch.sin(position * div_term)
-        w[:, 1::2] = torch.cos(position * div_term)
-
-        self.emb = nn.Embedding(c_in, d_model)
-        self.emb.weight = nn.Parameter(w, requires_grad=False)
-
-    def forward(self, x):
-        return self.emb(x).detach()
-
-class TemporalEmbedding(nn.Module):
-    def __init__(self, d_model, embed_type='fixed', freq='h'):
-        super(TemporalEmbedding, self).__init__()
-
-        minute_size = 4; hour_size = 24
-        weekday_size = 7; day_size = 32; month_size = 13
-
-        Embed = FixedEmbedding if embed_type=='fixed' else nn.Embedding
-        if freq=='t':
-            self.minute_embed = Embed(minute_size, d_model)
-        self.hour_embed = Embed(hour_size, d_model)
-        self.weekday_embed = Embed(weekday_size, d_model)
-        self.day_embed = Embed(day_size, d_model)
-        self.month_embed = Embed(month_size, d_model)
-    
-    def forward(self, x):
-        x = x.long()
-        
-        minute_x = self.minute_embed(x[:,:,4]) if hasattr(self, 'minute_embed') else 0.
-        hour_x = self.hour_embed(x[:,:,3])
-        weekday_x = self.weekday_embed(x[:,:,2])
-        day_x = self.day_embed(x[:,:,1])
-        month_x = self.month_embed(x[:,:,0])
-        
-        return hour_x + weekday_x + day_x + month_x + minute_x
-
-class TimeFeatureEmbedding(nn.Module):
-    def __init__(self, d_model, embed_type='timeF', freq='h'):
-        super(TimeFeatureEmbedding, self).__init__()
-
-        freq_map = {'h':4, 't':5, 's':6, 'm':1, 'a':1, 'w':2, 'd':3, 'b':3}
-        d_inp = freq_map[freq]
-        self.embed = nn.Linear(d_inp, d_model)
-    
-    def forward(self, x):
-        return self.embed(x)
-
-class DataEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
-        super(DataEmbedding, self).__init__()
-
-        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
-        self.position_embedding = PositionalEmbedding(d_model=d_model)
-        self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq) if embed_type!='timeF' else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
-
-        self.dropout = nn.Dropout(p=dropout)
-
-    def forward(self, x, x_mark):
-        x = self.value_embedding(x) + self.position_embedding(x) + self.temporal_embedding(x_mark)
-        
-        return self.dropout(x)
-
 class FullAttention(nn.Module):
     def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
         super(FullAttention, self).__init__()
@@ -210,7 +126,7 @@ class FullAttention(nn.Module):
     def forward(self, queries, keys, values, attn_mask):
         B, L, H, E = queries.shape
         _, S, _, D = values.shape
-        scale = self.scale or 1./sqrt(E)
+        scale = self.scale or 1./math.sqrt(E)
 
         scores = torch.einsum("blhe,bshe->bhls", queries, keys)
         if self.mask_flag:
@@ -329,10 +245,12 @@ class Model(nn.Module):
         d_model = 512
         embed = 'fixed'
         freq = 'h'
-        self.enc_embedding = DataEmbedding(x, d_model, embed, freq, dropout)
-        enc_out = self.enc_embedding(x, x)
-        attention_feature = self.attention(enc_out)
-        print(">>>>", ae_latent.shape, lstm_feature.shape, attention_feature.shape)
+        
+        enc_embedding = PositionalEmbedding(d_model)
+        enc_out = enc_embedding(x) # [1,50,512]
+        
+        attention_feature = self.attention(enc_out, enc_out, enc_out, False)
+        print(">>>>", ae_latent.shape, lstm_feature.shape, attention_feature[0].shape)
         raise
         '''
         seasonal_init = seasonal_init.squeeze()
