5140ae14f70080aef5481e9d62f866642dba52e9
xohyun <bluecow50@gmail.com>
2023-04-05 19:55:28 -0900

    score

--- Trainer/trainer_Boosting_aug.py
+++ Trainer/trainer_Boosting_aug.py
@@ -1,224 +1,231 @@
-import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import tqdm
+import wandb
+from tqdm import tqdm
+from sklearn.metrics import f1_score, precision_score, recall_score
+from utils.utils import gpu_checking, find_bundle
+from Trainer.base_trainer import base_trainer
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 
-class Pred_making():
-    '''
-    point adjust with quantile
-    '''
+class TrainMaker(base_trainer):
+    def __init__(self, args, model, data_loaders, data_info):
+        self.args = args
+        self.model = model
+        self.mode = self.args.mode
 
-    def quantile_score(self, true_list, errors):
-        l_quantile = np.quantile(np.array(errors), 0.0) # 0.025
-        u_quantile = np.quantile(np.array(errors), 0.95) # 0.975
-        in_range = np.logical_and(
-            np.array(errors) >= l_quantile, np.array(errors) <= u_quantile)
-        # pred_list = [0 for i in errors if i in in_range]
-        np_errors = np.array(errors)
-        # pred_list = [i for i in np_errors if i in np.where((i >= l_quantile and i <= u_quantile), 0, 1)]
-        pred_list = np.zeros(len(errors))
-        for i in range(len(np_errors)):
-            if errors[i] >= l_quantile and errors[i] <= u_quantile:
-                pred_list[i] = 0
-            else:
-                pred_list[i] = 1
+        if self.mode == "train" or self.mode == "all":
+            self.data_loader = data_loaders['train']
+            if self.args.valid_setting:
+                self.data_loader_valid = data_loaders['valid']
+        else:
+            self.data_loader_test = data_loaders['test']
 
-        true_list = np.array(true_list)
-        pred_list = np.array(pred_list)
+        self.device = gpu_checking(args)
 
-        return true_list, pred_list
-    
-    def _fixed_threshold(errors, k=3):
-        """
-        Calculate the threshold. (TadGAN)
-        The fixed threshold is defined as k standard deviations away from the mean.
-        Args:
-            errors (ndarray):
-                Array of errors.
-        Returns:
-            float:
-                Calculated threshold value.
-        """
-        mean = errors.mean()
-        std = errors.std()
+        self.features = data_info['num_features']
+        self.epoch = self.args.epoch
 
-        return mean + k * std
-    
-    def fix_threshold(self, true_list, errors):
-        threshold = self._fixed_threshold(errors)
-        pred_list = np.zeros(len(errors))
-        above_idx = np.where(np.array(errors) > threshold)
-        pred_list[above_idx] = 1
-        return true_list, pred_list
+        self.lr = self.args.lr
+        self.wd = self.args.wd
 
-    def distance_var_calc_score(self, true_list, dist_list):
-        # # target-feature 만의 variace를 계산해야 할 것인가 전체-feature를 할 것인가?
-        # # feature 간 또는 sequence 순서에 따른 모든 변동성에 차이를 고려할 것인가?
-        #  ** threshold =
-        # print(dist_list.shape)
-        # forecast_variances = torch.var(dist_list[:, 0], dim=1).numpy()
-        # # numpy array 라면 가로축(시간축)에 따라 각 feature 표준편차 계산
-        # # forecast_variances = np.var(dist_list[:,0], axis=-1, ddof=1)
-        # dist = np.sqrt((dist_list[:, 1] - forecast_variances)**2)
+        self.optimizer = getattr(torch.optim, self.args.optimizer)
+        self.optimizer = self.optimizer(
+            self.model.parameters(), lr=self.lr, weight_decay=self.wd)
 
-        #  ** pred_list = [n for n in dist if n > threshold else 0]
-        # # return true_list, pred_list
-        pass
+        self.criterion = self.set_criterion(self.args.criterion)
+        self.scheduler = self.set_scheduler(args, self.optimizer)
 
-    def variance_score(self, true_list, errors, dist_list):
-        _, pred_list_recon = self.quantile_score(true_list, errors)
-        
-        recon_var = dist_list['recon_var_list']
-        fore_var = dist_list['fore_var_list']
-        recon_var = torch.cat(recon_var)
-        fore_var = torch.cat(fore_var)
-        recon_var = torch.tensor(recon_var, device='cpu').numpy()
-        fore_var = torch.tensor(recon_var, device='cpu').numpy()
+        # self.__set_criterion(self.criterion)
+        # self.__set_scheduler(self.args, self.optimizer)
+        # self.cal = Calculate()
 
-        diff_var = abs(recon_var - fore_var)
-        pred_list = np.zeros(fore_var.shape[0])
-        for i in range(fore_var.shape[1]): # for feature num
-            d = diff_var[:,i]
-            u_quantile = np.quantile(np.array(d), 0.95) # 0.975
-            if d[i] >= u_quantile:
-                pred_list[i] = 1
-
-        print("--",sum(pred_list), len(pred_list))
-        print("##", sum(pred_list_recon), len(pred_list_recon))
-
-        sum_pred = [pred_list[i] + pred_list_recon[i] for i in range(len(pred_list))]
-        sum_pred = [1 if i > 0 else 0 for i in sum_pred]
-
-        pred_list = sum_pred
-        return true_list, pred_list
-    
-    def variance_score_with_weighted_sum(self, true_list, errors, dist_list):
-        error_sum = np.sum(errors, axis=1)
-        
-        recon_var = dist_list['recon_var_list'] # variance of reconstruct part
-        fore_var = dist_list['fore_var_list'] # variance of forecast part
-        recon_var = torch.cat(recon_var)
-        fore_var = torch.cat(fore_var)
-        recon_var = torch.tensor(recon_var, device='cpu').numpy() # numpy format
-        fore_var = torch.tensor(recon_var, device='cpu').numpy()
-
-        diff_var = abs(recon_var - fore_var)
-
-        new_error = 0.8*error_sum + 0.2*diff_var # weighted sum
-        pred_list = np.zeros(new_error.shape[0])
-        for i in range(fore_var.shape[1]): # for feature num
-            d = new_error[:,i]
-            u_quantile = np.quantile(np.array(d), 0.80) # 0.975
-            if d[i] >= u_quantile:
-                pred_list[i] = 1
-
-        print("--",sum(pred_list), len(pred_list))
-
-        return true_list, pred_list
-    
-    def variance_score_with_corr(self, args, true_list, errors, dist_list):
-        """
-        consider correlation of score
-        Args:
-            true_list:
-                Array of true answer
-            errors (ndarray):
-                Array of errors.
-            dist_list  : 
-                Array of distances.
-        Returns:
-            true_list, pred_list
-        """
-        #---# train data #---#
-        train_info = np.load(f'{args.save_path}train_output.npy', allow_pickle=True)
-        train_recon = []; train_fore = []; train_var = []; train_x = []
-        for i in range(len(train_info)):
-            data = train_info[i]
-            recon = data['reconstructs']; train_recon.append(recon)
-            fore = data['forecasts']; train_fore.append(fore)
-            var = data['variances']; train_var.append(var)
-            xs = data['x']; train_x.append(xs) 
-            # print(recon.shape, fore.shape, var.shape) # [16,50,25] / [16,13,25] / [16, 25]
-        train_recon = torch.cat(train_recon)
-        train_fore = torch.cat(train_fore)
-        train_var = torch.cat(train_var)
-        train_x = torch.cat(train_x)
-        recon_var = torch.var(train_recon, dim=1)
-        recon_error = abs(train_recon - train_x[:,:train_recon.shape[1],:]) # difference of recon
-        var_error = abs(train_var - recon_error) # difference of variance
-        print(train_recon.shape, train_fore.shape, train_var.shape, train_x.shape, recon_var.shape, "-=======")
-
-        relations = []
-        for i in range(len(recon_error)):
-            recon = recon_error[i] # [50,25]
-            var = var_error[i] # [25]
-            relation = recon * var # ?????
-            relations.append(relation)
-        relations = torch.stack(relations)
-        train_mm = torch.mean(relations, dim=0) # [50,25] 
-        
-        #---# test data #---#
-        recon_var = dist_list['recon_var_list']
-        fore_var = dist_list['fore_var_list']
-        recon_var = torch.cat(recon_var)
-        fore_var = torch.cat(fore_var)
-        recon_var = torch.tensor(recon_var, device='cpu').numpy()
-        fore_var = torch.tensor(recon_var, device='cpu').numpy()
-        
-        #---# difference of variances #---#
-        diff_var = abs(recon_var - fore_var)
-
-        # new_error = 0.8*error_sum + 0.2*diff_var # weighted sum
-        new_error = errors
-        print(new_error.shape,"------")
-
-        seq_len = errors.shape[1] # seq_len
-        
-        values = np.zeros((seq_len, seq_len))
-        for i in range(len(new_error)):
-            value = new_error[i] @ new_error[i].T
-            values =  values + value
+    def train(self, shuffle=True):
+        for e in tqdm(range(self.epoch)):
+            epoch_loss = 0
+            self.model.train()
             
-        '''pred_list = np.zeros(new_error.shape[0])
-        for i in range(fore_var.shape[1]): # for feature num
-            d = new_error[:,i]
-            u_quantile = np.quantile(np.array(d), 0.95) # 0.975
-            if d[i] >= u_quantile:
-                pred_list[i] = 1'''
-        print(values)
-        print(values.shape)
-        raise
-        print("--",sum(pred_list), len(pred_list))
 
+            for idx, x in enumerate(self.data_loader):
+                x = x.float().to(device=self.device)
+                self.optimizer.zero_grad()
+
+                forecast_part = x[:, int(self.args.seq_len*0.8):, :]
+                variances = torch.var(forecast_part, dim=1)
+
+                output = self.model(x)
+                output["real_variances"] = variances
+
+                pred = torch.concat(
+                    (output["reconstructs"], output["forecasts"]), dim=1)
+                loss = self.criterion(x, pred)
+                loss_var = self.criterion(variances, output["variances"])
+
+                #---# sum of variance loss and CEE #---#
+                loss = 0.8*loss + 0.2*loss_var
+
+                #---# Print loss #---#
+                interval = 300
+                if (idx+1) % interval == 0:
+                    print(f'[Epoch{e+1}] Loss:{loss}')
+
+                loss.backward()
+                self.optimizer.step()
+                epoch_loss += loss
+
+            #---# Model save #---#
+            torch.save(self.model.state_dict(),
+                       f'{self.args.save_path}model_{self.args.model}.pk')
+            print(f"{e}epoch / loss {loss}")
+
+            # wandb.log({"loss":loss})
+
+            if self.scheduler is not None:
+                self.scheduler.step()
+
+        # ---# To save trainer data #---#
+        outputs = []
+        with torch.no_grad():
+            for idx, x in enumerate(self.data_loader):
+                x = x.float().to(device=self.device)
+                output = self.model(x)
+                outputs.append(output)
+            outputs = np.array(outputs)
+            np.save(f'{self.args.save_path}train_output.npy', outputs)
+
+    def evaluation(self, test_loader):
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.model.eval()
+
+        dist_list = []
+
+        true_list = []
+        true_list_each = []
+        x_list = []
+        x_hat_list = []
+        errors = []
+        errors_each = []
+
+        recon_var_list = []; fore_var_list = []; 
+        with torch.no_grad():
+            for idx, (x, y) in enumerate(test_loader):
+                x = x.float().to(device=self.device)
+                self.optimizer.zero_grad()
+                batch = x.shape[0]
+
+                forecast_part = x[:, int(self.args.seq_len*0.8):, :]
+                variances = torch.var(forecast_part, dim=1)
+
+                output = self.model(x) # dictionary format
+                pred = torch.concat(
+                    (output["reconstructs"], output["forecasts"]), dim=1) # to make pred
+
+                # var_dist
+                pred_var = torch.var(pred, dim=1)
+                recon_var_list.append(output["variances"]) # reconstruct variance
+                fore_var_list.append(pred_var) # forecast variance
+                
+                # dist = [pred_var, output["variances"]]
+                # dist_list.append(dist)
+
+                error = torch.sum(abs(x - pred), axis=(1, 2)) # for every batch
+                errors.extend(error)
+                # errors_each.extend(
+                #     torch.sum(abs(x - pred), axis=2).reshape(-1))
+                errors_each.append(abs(x - pred)) # no sum
+
+                x_list.append(x)
+                x_hat_list.append(pred)
+
+                x = x.reshape(x.shape[0], -1)
+                pred = pred.reshape(pred.shape[0], -1)
+
+                true_list_each.extend(y.reshape(-1))
+                y = y.reshape(y.shape[0], -1)
+                y = y.mean(axis=1).numpy()
+                y = np.where(y > 0, 1, 0)
+                true_list.extend(y)
+
+        dist_list = {"recon_var_list":recon_var_list, "fore_var_list":fore_var_list}
+        errors = torch.tensor(errors, device='cpu').numpy()
+        errors_each = torch.cat(errors_each)
+        errors_each = torch.tensor(errors_each, device='cpu').numpy()
+
+        true_list, pred_list = self.get_score(self.args, self.args.score, true_list, errors, dist_list, errors_each)
+        f1, precision, recall = self.get_metric(self.args.calc, self.args, true_list, 
+                                                pred_list, true_list_each, errors_each)
+
+        return f1, precision, recall
+
+    def set_criterion(self, criterion):
+        if criterion == "MSE":
+            criterion = nn.MSELoss()
+        elif criterion == "CEE":
+            criterion = nn.CrossEntropyLoss()
+        elif criterion == "cosine":
+            criterion = nn.CosineEmbeddingLoss()
+        return criterion
+
+    def set_scheduler(self, args, optimizer):
+        if args.scheduler is None:
+            return None
+        elif args.scheduler == 'exp':
+            scheduler = optim.lr_scheduler.ExponentialLR(
+                optimizer, gamma=args.gamma)
+        elif args.scheduler == 'step':
+            scheduler = optim.lr_scheduler.StepLR(
+                optimizer, step_size=args.step_size, gamma=args.gamma)
+        elif args.scheduler == 'multi_step':
+            scheduler = optim.lr_scheduler.MultiStepLR(
+                optimizer, milestones=args.milestones, gamma=args.gamma)
+        elif args.scheduler == 'plateau':
+            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=20,
+                                                             threshold=0.1, threshold_mode='abs', verbose=True)
+        elif args.scheduler == 'cosine':
+            scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                             T_max=args.T_max if args.T_max else args.epochs,
+                                                             eta_min=args.eta_min if args.eta_min else 0)
+        elif args.scheduler == 'one_cycle':
+            scheduler = optim.lr_scheduler.OneCycleLR(optimizer,
+                                                      max_lr=args.max_lr,
+                                                      steps_per_epoch=args.steps_per_epoch,
+                                                      epochs=args.cycle_epochs)
+        else:
+            raise ValueError(f"Not supported {args.scheduler}.")
+        return scheduler
+
+    def get_score(self, args, method, true_list, errors, dist_list=None, errors_each=None):
+        from Score import make_pred
+        score_func = make_pred.Pred_making()
+
+        if method == 'quantile':
+            true_list, pred_list = score_func.quantile_score(true_list, errors)
+        elif method == 'variance':
+            true_list, pred_list = score_func.variance_score(true_list, errors, dist_list)
+        elif method == 'var_weight':
+            true_list, pred_list = score_func.variance_score_with_weighted_sum(true_list, errors_each, dist_list)
+        elif method == 'var_corr':
+            true_list, pred_list = score_func.variance_score_with_corr(args, true_list, errors_each, dist_list)
         return true_list, pred_list
 
-    def variance_score_online(self, true_list, errors, dist_list):
-        train_output = np.load(f'{self.args.save_path}train_output.npy')
-        train_recon = train_output['reconstruct']
-        train_variance = train_output['']
+    def get_metric(self, method, args, true_list, pred_list,
+                   true_list_each=None, errors_each=None):
+        # from Score import make_pred
+        from Score.calculate_score import Calculate_score
+        # score_func = make_pred.Pred_making()
+        metric_func = Calculate_score(args)
 
-        # 바로바로 output
-        
-        #---# train #---#
-        # train output의 distribution??을 이용해서 online detection에 이용
-        pass
-
-# TadGAN
-# errors, predictions_vs = reconstruction_errors(x_real, x_hat, score_window=self.args.seq_len, step_size=1) #score_window=config.window_size
-# error_range = find_anomalies(errors, index=range(len(errors)), anomaly_padding=5)
-# pred_list = np.zeros(len(true_list))
-# for i in error_range:
-#     start = int(i[0])
-#     end = int(i[1])
-#     pred_list[start:end] = 1
-
-# drawing(config, anomaly_value, pd.DataFrame(test_dataset.scaled_test))
-
-# batch 안에서 cosine similarity
-# diff = cos(x, pred).cpu().tolist()
-
-# batch_pred = np.where(((np.array(diff)<0) & (np.array(diff)>-0.1)), 1, 0)
-# batch_pred = np.where(np.array(diff)<0.7, 1, 0)
-# y = np.where(y>0.69, 1, 0)
-
-# diffs.extend(diff)
-# pred_list.extend(batch_pred)
+        if method == 'default':
+            f1, precision, recall = metric_func.score(true_list, pred_list)
+        elif method == 'back':
+            f1, precision, recall = metric_func.back_score(true_list, pred_list)
+        # var calc not ready
+        # elif method == 'variance':
+        #     f1, precision, recall = metric_func.score(true_list, errors, pred_list)
+        return f1, precision, recall
