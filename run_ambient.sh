8c103e681901b1badad5f2d73061b712ecd2d0e7
xohyun <bluecow50@gmail.com>
2023-04-12 10:16:18 -0900

    sh files

---
+++ run_ambient.sh
@@ -0,0 +1,10 @@
+for eee in 20 30 50 100 200 500
+do
+    for seq in 15 20 30 45 50 60
+    do
+        for step in 1 2 3 4 5 6
+        do
+            python main.py --csv_path="/content/drive/MyDrive/score/ambient/" --model=Boosting_aug --epoch=$eee --dataset=NAB --choice_data='ambient' --seq_len=$seq --step_len=$step --score='var_corr'
+        done
+    done
+done
